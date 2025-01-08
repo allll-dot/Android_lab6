891b5404648f34f99ea2938e6f5af23fe362e33b
LastProgram <145275888+LastProgram@users.noreply.github.com>
2025-01-09 05:55:40 +0700

    replace test data on query and use LiveData

--- app/src/main/java/com/example/alab6/database/CrimeDao.kt
+++ app/src/main/java/com/example/alab6/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package com.example.alab6.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.alab6.Crime
@@ -8,7 +9,7 @@ import java.util.UUID
 @Dao
 interface CrimeDao {
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 }
